22192c722647a9f9e91d7c1230f9d1d2ddd9100e
Nmlycette <78880054+Nmlycette@users.noreply.github.com>
2023-01-17 22:53:50 -0500

    creating mock data for product list

---
+++ CoffeeShopProductList/CoffeeShopProductList/CoffeeShopProductList/Models/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace CoffeeShopProductList.Models
+{
+    public class ProductViewModel
+    {
+        [DisplayName("Item Id")]
+        public int id { get; set; }
+
+        [DisplayName("Name")]
+        public string? name { get; set; }
+        
+        [DisplayName("Description")]
+        public string? description { get; set; }
+        
+        [DisplayName("Price")]
+        public double price { get; set; }
+
+        [DisplayName("Category")]
+        public string? category { get; set; }
+    }
+}
